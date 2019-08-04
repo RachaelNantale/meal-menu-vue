ff1c6c3353d863ed5a4743cbf4f7bc56654b45fc
RachaelNantale <rachaelnantale42@gmail.com>
2019-08-04 13:25:19 +0300

    add recipes

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,11 +1,17 @@
 <template>
     <span>
-        <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            class="brown lighten-2"
+            dark
+            disable-resize-watcher
+        >
             <v-list>
                 <template v-for="(item, index) in items">
                     <v-list-tile :key="index">
                         <v-list-tile-content>
-                            {{item.title}}
+                            {{ item.title }}
                         </v-list-tile-content>
                     </v-list-tile>
                     <v-divider :key="`divider-${index}`"></v-divider>
@@ -13,10 +19,13 @@
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-side-icon
+                class="hidden-md-and-up"
+                @click="drawer = !drawer"
+            ></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <router-link to="/">
-                <v-toolbar-title to="/">{{appTitle}}</v-toolbar-title>
+                <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
             </router-link>
             <v-btn text class="hidden-sm-and-down" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
@@ -24,8 +33,10 @@
                 <v-btn text to="/login">SIGN IN</v-btn>
                 <v-btn color="brown lighten-3" to="/register">JOIN</v-btn>
             </div>
-            <v-btn v-else outlined color="white" @click="logout">Logout</v-btn>
-
+            <div v-else>
+                <v-btn text to="/about">PROFILE</v-btn>
+                <v-btn outlined color="white" @click="logout">Logout</v-btn>
+            </div>
         </v-toolbar>
     </span>
 </template>
